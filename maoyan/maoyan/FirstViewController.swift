0d3f67a539f84bd7a59b7c41faeffcd242676556
王恒星 <star@wanghengxingdemacbook-pro.local>
2015-12-10 21:39:03 +0800

    swift

---
+++ maoyan/maoyan/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  maoyan
+//
+//  Created by 王恒星 on 15/12/10.
+//  Copyright © 2015年 maoyan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
